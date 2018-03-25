c1418b27ea4afdf70fd09355babb6e00398e4679
Velicaria <avelicaria@icloud.com>
2018-03-24 19:25:53 -0700

    add server

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'bye ms american pie' })
-})
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken)
+        })
+)
+
+// app.get('/auth/google', 
+//         passport.authenticate('google', {scope: ['profile', 'email']
+// }))
+
+app.get('/auth/google', function (request, response, next) {
+    passport.authenticate('google', { scope: ['profile', 'email'] })(request, response, next);
+});
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/profile',
+        failureRedirect: '/fail'
+    })
+)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
